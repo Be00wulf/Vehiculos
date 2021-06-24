a889bf83ac5f2a751a814c3f0df02049ca141b49
María José Baquiax <mariabaquiax201831422@cunoc.edu.gt>
2021-06-24 14:23:40 -0600

    SE HA MODIFICADO EL MAIN

---
+++ src/main/java/edu/cunoc/mavenhii/Principal.java
@@ -0,0 +1,13 @@
+package edu.cunoc.mavenhii;
+
+import edu.cunoc.vehiculos.ArregloVehiculo;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+        ArregloVehiculo vv1 = new ArregloVehiculo();
+        System.out.println(vv1);
+    }
+    
+}
